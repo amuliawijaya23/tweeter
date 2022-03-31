4fd87a4352fa7e56f51bb6f75400f9094a657755
amuliawijaya23 <amuliawijaya@gmail.com>
2022-03-31 01:17:45 -0700

    adde function to submit on enter and apply error validatioin message

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,64 +1,106 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
 $(document).ready(function () {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "/images/avatar1.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "/images/avatar2.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "/images/avatar1.png"
+  //       ,
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "/images/avatar2.png",
+  //       "handle": "@rd" },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ]
 
-  const renderTweets = function (tweets) {
-    for (const tweet of tweets) {
-      $('.feeds').append(createTweetElement(tweet));
-    }
-  };
+// *** Load and Render Tweets ***
 
   const createTweetElement = function (tweets) {
     let $tweet = $('<article>').addClass('tweets');
-  
+    
     //create header element
     let $user = $('<div>').append($('<img>').attr('src', tweets.user.avatars), $('<h2>').text(tweets.user.name))
     let $handle = ($('<a>').attr('href', '/')).append($('<p>').text(tweets.user.handle));
     let $header = $('<header>').append($user, $handle);
-  
-  
+    
+    
     // create content element
     let $content = $('<div>').addClass('tweet').append($('<p>').text(tweets.content.text));
-  
+    
     // create footer elements  
     let $dateAndLikes = $('<div>').append($('<p>').text(moment(tweets.created_at).fromNow()), $('<p>').attr('id', 'likeCounter').text('0 likes'));
     let $symbols = $('<div>').append($('<i>').addClass('fa-solid fa-heart').attr('id', 'like'), $('<i>').addClass('fa-solid fa-retweet').attr('id', 'retweet'), $('<i>').addClass('fa-solid fa-flag').attr('id', 'flag'))
     let $footer = $('<footer>').append($dateAndLikes, $symbols);
-  
+    
     return $tweet.append($header, $content, $footer);
   };
+  
+  const renderTweets = function (tweets) {
+    for (const tweet of tweets) {
+      $('.feeds').prepend(createTweetElement(tweet));
+    }
+  };
+  
+  // *** Form Submit new tweet to database ***
 
-  renderTweets(data);
+  const loadTweets = () => {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET'
+    }).done((tweet) => {
+      $('.feeds').empty();
+      renderTweets(tweet);
+    })
+  }
+  
+  loadTweets();
+  
+  const postTweet = () => {
+    if (!$('#tweet-text').val().trim()) {
+      $('#invalid').text('You cannot post an empty tweet!');
+      $('#invalid').css({'border': '2px solid red'}).show();
+      $('#tweet-text').val(''),focus();
+    } else if ($('#tweet-text').val().length > 140) {
+      $('#invalid').text('Your tweet is too long!');
+      $('#error').css({'border': '2px solid red'}).show();
+      $('#tweet-text').focus();
+    } else {     
+      $.ajax({
+        data: $('form').serialize(),
+        url: '/tweets',
+        method: 'POST'
+      }).done((tweet) => {
+        loadTweets();
+        $('#tweet-text').val('');
+        $('#tweet-text').focus();
+        $('#counter').val(140);
+      });
+    }
+  }
+
+  $('form').on('submit', (event) => {
+    // prevent refresh
+    event.preventDefault();
+    postTweet();
+  })
+
+  $('#tweet-text').keydown((event) => {
+    if (event.keyCode === 13 && !event.shiftKey) {
+      postTweet();
+    }
+  });
 });
-
 
 
