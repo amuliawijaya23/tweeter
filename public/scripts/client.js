3a1263f8cdda9b1932249d4661eef7a31fc8683e
amuliawijaya23 <amuliawijaya@gmail.com>
2022-03-31 23:01:23 -0700

    updated css for mobile first development

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,30 +1,5 @@
 $(document).ready(function () {
 
-  // const data = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": "/images/avatar1.png"
-  //       ,
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": "/images/avatar2.png",
-  //       "handle": "@rd" },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1461113959088
-  //   }
-  // ]
-
 // *** Load and Render Tweets ***
 
   const createTweetElement = function (tweets) {
